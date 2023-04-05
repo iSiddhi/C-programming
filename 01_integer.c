c4dc948b0b4945d13bd5ae065a64b969a91c0ae9
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2023-04-05 21:36:28 +0530

    Add files via upload

---
+++ 01_integer.c
@@ -0,0 +1,12 @@
+// Program to Print an Integer
+
+#include<stdio.h>
+int main(){
+    int number;
+    printf("enter the nuumber: ");
+    scanf("%d",&number);
+    printf("you entered: %d",number);
+
+    return 0;
+
+}
