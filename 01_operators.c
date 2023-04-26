db1236a2e99116732d13b363c42d5d55e03ab351
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2023-04-27 00:37:05 +0530

    Add files via upload

---
+++ 01_operators.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+// int main()
+// {
+//     int a,b;
+//     a=34;
+//     b=6;
+//     printf("a+b=  %d\n",a+b );
+//     printf("a-b=  %d\n",a-b);
+//     return 0;
+
+// }
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a,b;
+//     a=0;
+//     b=6;
+//     printf("a+b=  %d\n",a&&b );
+//     printf("a-b=  %d\n",a||b);
+//     // printf("a/b=  %d\n",a!b);
+
+//     return 0;
+
+// }
+
+
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    a=2;
+    b=3;
+    printf("a+b=  %d\n",a&b );
+    printf("a-b=  %d\n",a||b);
+    // printf("a/b=  %d\n",a!b);
+
+    return 0;
+
+}
+
+
+
+
