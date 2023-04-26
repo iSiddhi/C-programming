96d40605d6d691530ae8542e63d430cc198961ee
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2023-04-27 00:39:03 +0530

    Add files via upload

---
+++ 05_formtSpecifiers.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    int a=8;
+    float b=7.4444;
+    printf("the value of a is %d and value of b is %f",a,b);
+    return 0;
+}
---
+++ 06_IfElse.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a=2;
+    int b=1;
+if (a>b){
+    printf("hello");
+ } else{
+    printf("bye");
+    return 0;
+}
+}
+
